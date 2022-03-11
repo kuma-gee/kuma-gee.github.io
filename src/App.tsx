ba06ef1d95fa9a495a0ccf93ea901db6adbf8e24
myin <minyin142p@gmail.com>
2022-03-11 15:50:59 +0100

    feat: add tailwind + list games

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-enum State {
-  IN_DEVELOPMENT,
-  FINISHED,
-  ADDON,
-}
-
-const games = {
-  [State.IN_DEVELOPMENT]: ['dualist'],
-  [State.FINISHED]: ['suicide-hero', 'robo-soul'],
-  [State.ADDON]: ['godot-css-theme'],
+const games: { [s: string]: string[] } = {
+  'In Development': ['dualist'],
+  Finished: ['suicide-hero', 'robo-soul'],
+  Addon: ['godot-css-theme'],
 };
 
 function App() {
+  const gameItems = Object.keys(games).map((state) => {
+    const items = games[state].map((game) => {
+      return (
+        <li>
+          <a href={'https://github.com/kuma-gee/' + game}>{game}</a>
+        </li>
+      );
+    });
+
+    return (
+      <article>
+        <h2 className="text-xl font-bold">{state}</h2>
+        <ul>{items}</ul>
+      </article>
+    );
+  });
+
   return (
-    <div className="App">
-      <header>
-        <h1>Kuma Gee</h1>
+    <div className="flex flex-col items-center">
+      <header className="flex flex-col items-center">
+        <h1 className="text-3xl font-bold">Kuma Gee</h1>
         <p>wannabe indie game developer</p>
-        <img src="logo.png" alt="logo" />
+        <img src="logo.png" alt="logo" className="rounded-full w-1/2" />
       </header>
+
+      {gameItems}
     </div>
   );
 }
