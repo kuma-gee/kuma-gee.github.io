d3f528b17ab034180aa422a0620db55ceebe0e78
myin <minyin142p@gmail.com>
2023-08-18 20:44:26 +0200

    add policy

---
+++ src/routes/policy-launcher.tsx
@@ -0,0 +1,18 @@
+export function PolicyLauncher() {
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-4 mx-auto">
+        <h1 className="font-bold text-2xl">Privacy policy for Min Launcher</h1>
+
+        <p>
+          The app "Min Launcher" is collecting{" "}
+          <strong>no personal information</strong> about the user in any way.
+          The only data saved on the device is supplied by the user itself while
+          using the app. Even in case of accidental saving of personal
+          information, all data does not leave the device itself and is not
+          collected on any servers.
+        </p>
+      </div>
+    </div>
+  );
+}
