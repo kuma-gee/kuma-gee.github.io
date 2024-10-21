9781da362400fc60efa542509eb9195693a470da
myin <minyin142p@gmail.com>
2024-10-21 15:34:10 +0200

    use construction sign

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 function App() {
   return (
     <div
-      className="w-full h-full flex flex-col gap-10 text-white text-center"
+      className="w-full h-full flex flex-col gap-16 text-white text-center"
       style={{
         backgroundImage: "url(/banner.png)",
         backgroundSize: "100vw auto",
         animation: "moveIt 90s linear infinite",
       }}
     >
-      <div className="flex flex-col grow gap-8 100vh">
+      <div className="flex flex-col gap-8">
         <img
           className="rounded-full max-w-[12rem] self-center mt-24"
           src="/kuma-gee.png"
@@ -20,7 +20,11 @@ function App() {
           <span className="text-sm font-light">Indie Game Developer</span>
         </div>
       </div>
-      <div className="p-2">\.Under Construction./</div>
+
+      <div className="flex flex-col gap-4 justify-center items-center p-4">
+        <img src="/construction.png" alt="construction sign" className="w-32"/>
+        <img src="/construction-kuma.png" alt="construction bear bowing" className="w-32"/>
+      </div>
     </div>
   );
 }
